499f471a6d1a72f81b0e287f015890534c584191
Leland Boeman <lboeman@email.arizona.edu>
2018-10-16 15:56:36 -0700

    reorganization and updateS

--- serve.py
+++ serve.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+import pdb
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import tornado.ioloop
@@ -13,10 +14,23 @@ class BaseHandler(RequestHandler):
             loader=FileSystemLoader(str(template_dir)),
             autoescape=select_autoescape([])
         )
+    def make_breadcrumb_html(self):
+        path = self.request.uri
+        parts = path.split('/')
+        breadcrumb = ""
+        for idx, part in enumerate(parts):
+            if part == "":
+                continue
+            breadcrumb += '/<a href="{}">{}</a>'.format('/'.join(parts[:idx+1]), part)
+        return breadcrumb
 
     def get(self):
         template = self.env.get_template(self.template)
-        rendered = template.render()
+        if hasattr(self, 'subnav') and self.subnav is not None:
+            subnav = self.subnav
+        else:
+            subnav = {}
+        rendered = template.render(breadcrumb=self.make_breadcrumb_html(), current_path=self.request.uri, subnav=subnav)
         self.write(rendered)
 
 
@@ -24,30 +38,45 @@ class OrgHandler(BaseHandler):
     template = 'org/obs.html'
 
 
-class DataHandler(BaseHandler):
-    template = 'data/data.html'
+class SiteHandler(BaseHandler):
+    template = 'data/site.html'
+    subnav = {
+        "/tep/avalon_2": "Data",
+        }
 
 
-class AccessHandler(BaseHandler):
+class DataDashHandler(BaseHandler):
+    subnav = { 
+        "/tep/avalon_2/ac_power": "Data",
+        "/tep/avalon_2/ac_power/access": "Access",
+        "/tep/avalon_2/ac_power/trials": "Active Trials",
+        "/tep/avalon_2/ac_power/reports": "Reports",
+    }
+
+class DataHandler(DataDashHandler):
+    template = 'data/asset.html'
+
+class AccessHandler(DataDashHandler):
     template = 'data/access.html'
 
 
-class ReportsHandler(BaseHandler):
+class ReportsHandler(DataDashHandler):
     template = 'data/reports.html'
 
 
-class TrialsHandler(BaseHandler):
+class TrialsHandler(DataDashHandler):
     template = 'data/trials.html'
 
 
 logger=logging.getLogger()
 logger.setLevel(logging.DEBUG)
 static_files = Path(__file__).parent / 'static'
-app = Application([(r'/', OrgHandler),
-                   (r'/data', DataHandler),
-                   (r'/trials', TrialsHandler),
-                   (r'/access', AccessHandler),
-                   (r'/reports', ReportsHandler),],
+app = Application([(r'/tep', OrgHandler),
+                   (r'/tep/avalon_2', SiteHandler),
+                   (r'/tep/avalon_2/ac_power', DataHandler),
+                   (r'/tep/avalon_2/ac_power/trials', TrialsHandler),
+                   (r'/tep/avalon_2/ac_power/access', AccessHandler),
+                   (r'/tep/avalon_2/ac_power/reports', ReportsHandler),],
                    static_path=str(static_files),
                    autoreload=True)
 
