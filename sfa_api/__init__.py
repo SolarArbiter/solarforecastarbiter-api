8045b8db7d5f1f14c3af25e9476f9a268517e086
Tony Lorenzo <atlorenzo@email.arizona.edu>
2018-11-06 16:07:54 -0700

    add flask talisman

--- sfa_api/__init__.py
+++ sfa_api/__init__.py
@@ -5,18 +5,21 @@ del get_versions
 
 from flask import Flask, Response, jsonify, render_template, url_for  # NOQA
 from flask_marshmallow import Marshmallow  # NOQA
+from flask_talisman import Talisman  # NOQA
 
 
 from sfa_api.spec import spec   # NOQA
 
 
 ma = Marshmallow()
+talisman = Talisman()
 
 
 def create_app(config_name='ProductionConfig'):
     app = Flask(__name__)
     app.config.from_object(f'sfa_api.config.{config_name}')
     ma.init_app(app)
+    talisman.init_app(app)
 
     from sfa_api.observations import obs_blp
     from sfa_api.sites import site_blp
@@ -36,10 +39,14 @@ def create_app(config_name='ProductionConfig'):
     def get_apispec_json():
         return jsonify(spec.to_dict())
 
+    redoc_script = f"https://cdn.jsdelivr.net/npm/redoc@{app.config['REDOC_VERSION']}/bundles/redoc.standalone.js"  # NOQA
+
     @app.route('/docs')
+    @talisman(content_security_policy={
+        'script-src': f"{redoc_script} 'self' blob:",
+    })
     def render_docs():
         return render_template('doc.html',
                                apispec_path=url_for('get_apispec_json'),
-                               redoc_version=app.config['REDOC_VERSION'])
-
+                               redoc_script=redoc_script)
     return app
