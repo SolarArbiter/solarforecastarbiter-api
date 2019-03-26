21a8b370351d00913d4dd922749c65ac3df56946
Tony Lorenzo <atlorenzo@email.arizona.edu>
2019-03-26 11:01:41 -0700

    add test data to db

---
+++ datastore/migrations/0015_test_data.down.sql
@@ -0,0 +1,3 @@
+DELETE FROM arbiter_data.organizations WHERE id = UUID_TO_BIN('b76ab62e-4fe1-11e9-9e44-64006a511e6f', 1);
+DROP PROCEDURE arbiter_data.insertfx;
+DROP PROCEDURE arbiter_data.insertobs;
