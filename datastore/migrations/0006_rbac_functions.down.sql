b22b260253d40a85c664c0b2afbb4ffd0e581538
Tony Lorenzo <atlorenzo@email.arizona.edu>
2019-03-27 15:33:51 -0700

    add organization_name function to support "provider"

--- datastore/migrations/0006_rbac_functions.down.sql
+++ datastore/migrations/0006_rbac_functions.down.sql
@@ -3,3 +3,4 @@ DROP PROCEDURE list_objects_user_can_read;
 DROP FUNCTION can_user_perform_action;
 DROP FUNCTION user_can_create;
 DROP FUNCTION get_user_organization;
+DROP FUNCTION get_organization_name;
--- datastore/migrations/0006_rbac_functions.up.sql
+++ datastore/migrations/0006_rbac_functions.up.sql
@@ -79,6 +79,14 @@ READS SQL DATA SQL SECURITY DEFINER
 RETURN (SELECT organization_id FROM arbiter_data.users WHERE auth0_id = auth0id);
 
 
+-- function to get organization name
+CREATE DEFINER = 'select_rbac'@'localhost' FUNCTION get_organization_name (orgid BINARY(16))
+RETURNS VARCHAR(32)
+COMMENT 'Return the name of the organization'
+READS SQL DATA SQL SECURITY DEFINER
+RETURN (SELECT name from arbiter_data.organizations WHERE id = orgid);
+
+
 -- Grant only required permissions to limited user account to execute rbac functions
 GRANT SELECT ON `arbiter_data`.`permission_object_mapping` TO `select_rbac`@`localhost`;
 GRANT SELECT ON `arbiter_data`.`permissions` TO `select_rbac`@`localhost`;
@@ -86,7 +94,9 @@ GRANT SELECT ON `arbiter_data`.`roles` TO `select_rbac`@`localhost`;
 GRANT SELECT ON `arbiter_data`.`role_permission_mapping` TO `select_rbac`@`localhost`;
 GRANT SELECT ON `arbiter_data`.`user_role_mapping` TO `select_rbac`@`localhost`;
 GRANT SELECT ON `arbiter_data`.`users` TO `select_rbac`@`localhost`;
+GRANT SELECT ON `arbiter_data`.`organizations` TO `select_rbac`@`localhost`;
 GRANT EXECUTE ON PROCEDURE `arbiter_data`.`list_objects_user_can_read` TO `select_rbac`@`localhost`;
 GRANT EXECUTE ON FUNCTION `arbiter_data`.`can_user_perform_action` TO `select_rbac`@`localhost`;
 GRANT EXECUTE ON FUNCTION `arbiter_data`.`user_can_create` TO `select_rbac`@`localhost`;
 GRANT EXECUTE ON FUNCTION `arbiter_data`.`get_user_organization` TO `select_rbac`@`localhost`;
+GRANT EXECUTE ON FUNCTION `arbiter_data`.`get_organization_name` TO `select_rbac`@`localhost`;
