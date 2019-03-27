49f4e56c21a8ce7905398385aebbb83e2eaffa7e
Tony Lorenzo <atlorenzo@email.arizona.edu>
2019-03-27 16:15:03 -0700

    update sites demo

--- sfa_api/demo/sites.py
+++ sfa_api/demo/sites.py
@@ -1,12 +1,25 @@
 static_sites = {
     '123e4567-e89b-12d3-a456-426655440001': {
         "elevation": 595.0,
-        "extra_parameters": {
-            '{"network_api_abbreviation": "AS","network": "University of Oregon SRML","network_api_id": "94040"}', # NOQA
-        },
+        "extra_parameters": (
+            '{"network_api_abbreviation": "AS","network": "University of Oregon SRML","network_api_id": "94040"}' # NOQA
+        ),
         "latitude": 42.19,
         "longitude": -122.7,
         "modeling_parameters": {
+            "ac_capacity": None,
+            "ac_loss_factor": None,
+            "axis_azimuth": None,
+            "axis_tilt": None,
+            "backtrack": None,
+            "dc_capacity": None,
+            "dc_loss_factor": None,
+            "ground_coverage_ratio": None,
+            "max_rotation_angle": None,
+            "surface_azimuth": None,
+            "surface_tilt": None,
+            "temperature_coefficient": None,
+            "tracking_type": None
         },
         "name": "Ashland OR",
         "provider": "Reference",
@@ -15,12 +28,24 @@ static_sites = {
     },
     'd2018f1d-82b1-422a-8ec4-4e8b3fe92a4a': {
         "elevation": 786.0,
-        "extra_parameters": {
-            '{"network": "NREL MIDC"}',
-        },
+        "extra_parameters": '{"network": "NREL MIDC"}',
         "latitude": 32.22969,
         "longitude": -110.95534,
-        "modeling_parameters": {},
+        "modeling_parameters": {
+            "ac_capacity": None,
+            "ac_loss_factor": None,
+            "axis_azimuth": None,
+            "axis_tilt": None,
+            "backtrack": None,
+            "dc_capacity": None,
+            "dc_loss_factor": None,
+            "ground_coverage_ratio": None,
+            "max_rotation_angle": None,
+            "surface_azimuth": None,
+            "surface_tilt": None,
+            "temperature_coefficient": None,
+            "tracking_type": None
+        },
         "name": "Weather Station 1",
         "provider": "Reference",
         "timezone": "America/Phoenix",
@@ -28,17 +53,23 @@ static_sites = {
     },
     '123e4567-e89b-12d3-a456-426655440002': {
         "elevation": 786.0,
-        "extra_parameters": {
-        },
+        "extra_parameters": "",
         "latitude": 43.73403,
         "longitude": -96.62328,
         "modeling_parameters": {
             "ac_capacity": 0.015,
+            "ac_loss_factor": 0.0,
+            "axis_azimuth": None,
+            "axis_tilt": None,
+            "backtrack": None,
             "dc_capacity": 0.015,
-            "temperature_coefficient": -.002,
-            "surface_azimuth": 180,
+            "dc_loss_factor": 0.0,
+            "ground_coverage_ratio": None,
+            "max_rotation_angle": None,
+            "surface_azimuth": 180.0,
             "surface_tilt": 45.0,
-            "tracking_type": "Fixed"
+            "temperature_coefficient": -.002,
+            "tracking_type": "fixed"
         },
         "name": "Power Plant 1",
         "provider": "Reference",
