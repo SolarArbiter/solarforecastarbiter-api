e875cf074310a4bce0638b0f6cfb8d0a05fd9441
Tony Lorenzo <atlorenzo@email.arizona.edu>
2019-04-01 10:23:20 -0700

    add permissions

--- datastore/migrations/0015_test_data.down.sql
+++ datastore/migrations/0015_test_data.down.sql
@@ -2,4 +2,5 @@ DELETE FROM arbiter_data.organizations WHERE id = UUID_TO_BIN('876abd2e-9fe1-11e
 DELETE FROM arbiter_data.organizations WHERE id = UUID_TO_BIN('b76ab62e-4fe1-11e9-9e44-64006a511e6f', 1);
 DROP PROCEDURE arbiter_data.insertfx;
 DROP PROCEDURE arbiter_data.insertobs;
+DROP PROCEDURE arbiter_data.insertcdffx;
 DROP USER 'apiuser'@'%';
