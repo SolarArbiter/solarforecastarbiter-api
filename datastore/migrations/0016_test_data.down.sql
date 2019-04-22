dc47fe326506f0cef6e4dbea0ec9035de8615265
Leland Boeman <lboeman@email.arizona.edu>
2019-04-22 09:28:30 -0700

    fix 0016_test_data.down.sql: remove comments, correct table names

--- datastore/migrations/0016_test_data.down.sql
+++ datastore/migrations/0016_test_data.down.sql
@@ -1,7 +1,3 @@
 DELETE FROM arbiter_data.observations_values;
-DELETE from arbiter_data.forecast_values;
-#DROP PROCEDURE insertghiobs;
-#DROP PROCEDURE insertghifx;
-#DROP PROCEDURE insertghicdffx;
-#DROP PROCEDURE insert1minghifx;
-#DROP PROCEDURE insert1minghicdffx;
+DELETE FROM arbiter_data.forecasts_values;
+DELETE FROM arbiter_data.cdf_forecasts_values;
