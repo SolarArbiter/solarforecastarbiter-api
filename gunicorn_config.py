e343649846a286f92f0ef2555216bc44bf948456
Tony Lorenzo <atlorenzo@email.arizona.edu>
2019-05-20 10:35:03 -0700

    update gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 
 
@@ -6,12 +6,11 @@ bind = '127.0.0.1:8080'
 workers = 8
 worker_class = 'gevent'
 keep_alive = 5
-if not os.getenv('SFAAPI_INSECURE', False):
-    ssl_version = 5
-    certfile = '/certs/tls.crt'
-    keyfile = '/certs/tls.key'
-# remote_address - user date "status line" status response_length "referer" "user agent" "X-Forwarded-For" request_time (s)  # NOQA
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Forwarded-For}i)s" %(L)s'  # NOQA
+ssl_version = 5
+certfile = '/certs/tls.crt' if Path('/certs/tls.crt').exists() else None
+keyfile = '/certs/tls.key' if Path('/certs/tls.key').exists() else None
+# date X-Forwarded-For status "status line" response_length "referer" "user agent" request_time (s)  # NOQA
+access_log_format = '%(t)s %({X-Forwarded-For}i)s %(s)s "%(r)s" %(b)s "%(f)s" "%(a)s"  %(L)s'  # NOQA
 accesslog = '-'
 
 
