7d558acda3a808accae81c5880c7b175ff411951
Leland Boeman <lboeman@email.arizona.edu>
2019-06-05 10:23:11 -0700

    Layout fixes (#93)
    * responsive updates, stop timedelta fields wrapping, stack sidebar on small screens
    
    * close button
    
    * stack labels of start/end on download form so that date/time don't wrap separately
    
    * explicitly set form-group width
    
    * add X box to close organizaiton filter
    
    * indicate timezone in forecast metadata field
    
    * move from fixed to relative header nav on small screens

--- sfa_dash/static/js/filter_column.js
+++ sfa_dash/static/js/filter_column.js
@@ -2,22 +2,26 @@
  * class and creates a drop-down list of checkboxes for hiding table rows by organizaiton
  */
 $(document).ready(function() {
-    var availableOrgs = new Set([]);
-    var orgs = $(".provider-column");
-    for (i = 0; i < orgs.length; i++) {
-        availableOrgs.add(orgs[i].textContent);
-    }
-    var filter_div = $("<div id='org-filters' class='collapse'>Filter by Organization <br/><hr><ul class='org-filter-options'></ul></div>");
-    filter_div.appendTo("#provider-header");
-    availableOrgs.forEach(function (e) {$(".org-filter-options").append(`<li><input class="org-filter-option"value="${e}" type="checkbox" checked>${e}</li>`)});
-    
-    $(".org-filter-option").change(function() {
-        console.log(`${this.checked}`);
-        console.log(` content: ${this.value}`);
-        if (this.checked) {
-            $(`.provider-column:contains("${this.value}")`).parent().show();
-        } else {
-           $(`.provider-column:contains("${this.value}")`).parent().hide();
+    if ($('#provider-header').length){
+        var availableOrgs = new Set([]);
+        var orgs = $(".provider-column");
+        for (i = 0; i < orgs.length; i++) {
+            availableOrgs.add(orgs[i].textContent);
         }
-    });
+        var filter_div = $("<div id='org-filters' class='collapse'>Filter by Organization <a href='#' role='button' id='org-filter-collapse'></a><br/><hr><ul class='org-filter-options'></ul></div>");
+        filter_div.appendTo("#provider-header");
+        availableOrgs.forEach(function (e) {$(".org-filter-options").append(`<li><input class="org-filter-option"value="${e}" type="checkbox" checked>${e}</li>`)});
+        $('#org-filter-collapse').click(function() {
+            $('#org-filters').collapse('toggle');
+        });
+        $(".org-filter-option").change(function() {
+            console.log(`${this.checked}`);
+            console.log(` content: ${this.value}`);
+            if (this.checked) {
+                $(`.provider-column:contains("${this.value}")`).parent().show();
+            } else {
+               $(`.provider-column:contains("${this.value}")`).parent().hide();
+            }
+        });
+    }
 });
