c63250e3d27507ce48e6ed5ffeb09dca817ea261
Leland Boeman <lboeman@email.arizona.edu>
2019-08-29 13:38:29 -0700

    refactor procedures, add default roles to existing, manually inserted users

--- datastore/migrations/0023_data_sharing.up.sql
+++ datastore/migrations/0023_data_sharing.up.sql
@@ -70,6 +70,7 @@ BEGIN
                 AND organization_id = get_organization_id('Reference'));
 END;
 GRANT EXECUTE ON FUNCTION arbiter_data.get_reference_role_id TO 'select_rbac'@'localhost';
+GRANT EXECUTE ON FUNCTION arbiter_data.get_reference_role_id TO 'insert_rbac'@'localhost';
 
 
 /*
@@ -90,6 +91,47 @@ END;
 GRANT EXECUTE ON FUNCTION arbiter_data.role_contains_rbac_permissions TO 'select_rbac'@'localhost';
 GRANT EXECUTE ON FUNCTION arbiter_data.role_contains_rbac_permissions TO 'insert_rbac'@'localhost';
 
+
+/*
+ * Drop and redefine add_permission_to_role to limit ability to add
+ * rbac permissions to roles assigned to external users
+ */
+DROP PROCEDURE add_permission_to_role;
+
+CREATE DEFINER = 'insert_rbac'@'localhost' PROCEDURE add_permission_to_role (
+    IN auth0id VARCHAR(32), IN role_id CHAR(36), IN permission_id CHAR(36))
+COMMENT 'Add an permission to the role permission mapping table'
+MODIFIES SQL DATA SQL SECURITY DEFINER
+BEGIN
+    DECLARE allowed BOOLEAN DEFAULT FALSE;
+    DECLARE roleid BINARY(16);
+    DECLARE permid BINARY(16);
+    DECLARE userorg BINARY(16);
+    SET userorg = get_user_organization(auth0id);
+    SET roleid = UUID_TO_BIN(role_id, 1);
+    SET permid = UUID_TO_BIN(permission_id, 1);
+    -- Check if user has update permission on the role and that
+    -- role and permission have same organization
+    SET allowed = can_user_perform_action(auth0id, roleid, 'update') AND
+        userorg = get_object_organization(permid, 'permissions') AND
+        userorg = get_object_organization(roleid, 'roles');
+    IF allowed IS NOT NULL AND allowed THEN
+        -- Don't insert an rbac permission when a role is granted externally
+        IF rbac_permissions_check(permid) AND role_granted_to_external_users(roleid) THEN
+            SIGNAL SQLSTATE '42000' SET MESSAGE_TEXT = 'Cannot add administrative permissions to role of external users',
+            MYSQL_ERRNO = 1142;
+        ELSE
+            INSERT INTO arbiter_data.role_permission_mapping (
+                role_id, permission_id) VALUES (roleid, permid);
+        END IF;
+    ELSE
+        SIGNAL SQLSTATE '42000' SET MESSAGE_TEXT = 'Access denied to user on "add permission to role"',
+        MYSQL_ERRNO = 1142;
+    END IF;
+END;
+
+GRANT EXECUTE ON PROCEDURE arbiter_data.add_permission_to_role TO 'insert_rbac'@'localhost';
+GRANT EXECUTE ON PROCEDURE arbiter_data.add_permission_to_role TO 'apiuser'@'%';
 
 /*
  * Drop and redefine create_role to include permission to read the role by default
@@ -233,54 +275,108 @@ INSERT INTO arbiter_data.users (id, auth0_id, organization_id) VALUES(
 
 
 /*
+ * Create a default user role allowing the user to read themselves, and the
+ * role.
+ */
+CREATE DEFINER = 'insert_rbac'@'localhost' PROCEDURE create_default_user_role(
+    IN userid BINARY(16))
+COMMENT 'Creates a default role for a user, granting a read self permission'
+MODIFIES SQL DATA SQL SECURITY DEFINER
+BEGIN
+    DECLARE orgid BINARY(16);
+    DECLARE roleid BINARY(16);
+    DECLARE rolename VARCHAR(64);
+    DECLARE userperm BINARY(16);
+    DECLARE roleperm BINARY(16);
+    SET orgid = get_organization_id('Unaffiliated');
+    SET roleid = UUID_TO_BIN(UUID(), 1);
+    SET rolename = CONCAT('User role ', BIN_TO_UUID(userid, 1));
+    SET userperm = UUID_TO_BIN(UUID(), 1);
+    SET roleperm = UUID_TO_BIN(UUID(), 1);
+    INSERT INTO arbiter_data.roles(name, description, id, organization_id
+    ) VALUES (rolename, 'Default role', roleid, orgid);
+    INSERT INTO arbiter_data.user_role_mapping(user_id, role_id
+    ) VALUES (userid, roleid);
+    INSERT INTO arbiter_data.permissions(id, description, organization_id, action, object_type
+    ) VALUES (
+    userperm, CONCAT('Read Self User ', BIN_TO_UUID(userid, 1)), orgid, 'read', 'users');
+    INSERT INTO arbiter_data.role_permission_mapping(permission_id, role_id
+    ) VALUES (userperm, roleid);
+    INSERT INTO arbiter_data.permission_object_mapping(permission_id, object_id
+    ) VALUES (userperm, userid);
+    INSERT INTO arbiter_data.permissions(id, description, organization_id, action, object_type
+    ) VALUES(
+    roleperm, CONCAT('Read User Role ', BIN_TO_UUID(roleid, 1)), orgid, 'read', 'roles');
+    INSERT INTO arbiter_data.role_permission_mapping(permission_id, role_id
+    ) VALUES (roleperm, roleid);
+    INSERT INTO arbiter_data.permission_object_mapping(permission_id, object_id
+    ) VALUES (roleperm, roleid);
+END;
+GRANT EXECUTE ON PROCEDURE arbiter_data.create_default_user_role TO 'insert_rbac'@'localhost';
+
+
+/*
+ * Grant a user access to the reference data set
+ */
+CREATE DEFINER = 'insert_rbac'@'localhost' PROCEDURE add_reference_role_to_user(
+    IN userid BINARY(16))
+MODIFIES SQL DATA SQL SECURITY DEFINER
+BEGIN
+    INSERT INTO arbiter_data.user_role_mapping (user_id, role_id) VALUES (
+            userid, get_reference_role_id());
+END;
+GRANT EXECUTE ON PROCEDURE arbiter_data.add_reference_role_to_user TO 'insert_rbac'@'localhost';
+
+
+/*
+ * Update existing users with default role and reference role
+ */
+CREATE PROCEDURE add_defaults_to_existing_users()
+MODIFIES SQL DATA
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE userid BINARY(16);
+
+    DECLARE cur CURSOR FOR SELECT id FROM arbiter_data.users;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO userid;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+        CALL arbiter_data.create_default_user_role(userid);
+    END LOOP;
+
+    CLOSE cur;
+END;
+
+-- Call and remove the procedure for updating existing users
+CALL add_defaults_to_existing_users();
+DROP PROCEDURE add_defaults_to_existing_users;
+
+
+/*
  * If the calling user does not exist, create a new user and add them to the
  * database under the 'unaffiliated' org grant them the Reference reading role
  */
 CREATE DEFINER = 'insert_rbac'@'localhost' PROCEDURE create_user_if_not_exists(IN auth0id VARCHAR(32))
-COMMENT 'Inserts a new user and adds then to the Public org, and read reference role'
+COMMENT 'Inserts a new user and adds them to the Unaffiliated org, and read reference role'
 READS SQL DATA SQL SECURITY DEFINER
 BEGIN
     DECLARE userid BINARY(16);
-    DECLARE roleid BINARY(16);
-    DECLARE rolename VARCHAR(64);
-    DECLARE orgid BINARY(16);
-    DECLARE userperm BINARY(16);
-    DECLARE roleperm BINARY(16);
     IF NOT does_user_exist(auth0id) THEN
         SET userid = UUID_TO_BIN(UUID(), 1);
-        SET orgid = get_organization_id('Unaffiliated');
         INSERT INTO arbiter_data.users (id, auth0_id, organization_id) VALUES (
-            userid, auth0id, orgid);
-        INSERT INTO arbiter_data.user_role_mapping (user_id, role_id) VALUES (
-            userid, get_reference_role_id());
-        -- Create the default role
-        SET roleid = UUID_TO_BIN(UUID(), 1);
-        SET rolename = CONCAT('User role ', BIN_TO_UUID(userid, 1));
-        SET userperm = UUID_TO_BIN(UUID(), 1);
-        SET roleperm = UUID_TO_BIN(UUID(), 1);
-        INSERT INTO arbiter_data.roles(name, description, id, organization_id
-        ) VALUES (rolename, 'Default role', roleid, orgid);
-        INSERT INTO arbiter_data.user_role_mapping(user_id, role_id
-        ) VALUES (userid, roleid);
-        INSERT INTO arbiter_data.permissions(id, description, organization_id, action, object_type
-        ) VALUES (
-        userperm, CONCAT('Read Self User ', BIN_TO_UUID(userid, 1)), orgid, 'read', 'users');
-        INSERT INTO arbiter_data.role_permission_mapping(permission_id, role_id
-        ) VALUES (userperm, roleid);
-        INSERT INTO arbiter_data.permission_object_mapping(permission_id, object_id
-        ) VALUES (userperm, userid);
-        INSERT INTO arbiter_data.permissions(id, description, organization_id, action, object_type
-        ) VALUES(
-        roleperm, CONCAT('Read User Role ', BIN_TO_UUID(roleid, 1)), orgid, 'read', 'roles');
-        INSERT INTO arbiter_data.role_permission_mapping(permission_id, role_id
-        ) VALUES (roleperm, roleid);
-        INSERT INTO arbiter_data.permission_object_mapping(permission_id, object_id
-        ) VALUES (roleperm, roleid);
+            userid, auth0id, get_organization_id('Unaffiliated'));
+        CALL arbiter_data.add_reference_role_to_user(userid);
+        CALL arbiter_data.create_default_user_role(userid);
     END IF;
 END;
 GRANT EXECUTE ON PROCEDURE arbiter_data.create_user_if_not_exists TO 'insert_rbac'@'localhost';
 GRANT EXECUTE ON FUNCTION arbiter_data.does_user_exist TO 'insert_rbac'@'localhost';
-GRANT EXECUTE ON FUNCTION arbiter_data.get_reference_role_id TO 'insert_rbac'@'localhost';
 
 GRANT EXECUTE ON PROCEDURE arbiter_data.create_user_if_not_exists TO 'apiuser'@'%';
 
@@ -292,14 +388,19 @@ CREATE DEFINER = 'select_rbac'@'localhost' PROCEDURE get_current_user_info (
     IN auth0id VARCHAR(32))
 COMMENT 'Read the identifying information for the current user.'
 BEGIN
-    SELECT get_organization_name(organization_id) as organization,
-        BIN_TO_UUID(organization_id, 1) as organization_id,
-        BIN_TO_UUID(id, 1) as user_id, auth0_id
-    FROM arbiter_data.users WHERE auth0_id = auth0id;
+    DECLARE userid CHAR(36);
+    SET userid = (SELECT BIN_TO_UUID(id, 1) FROM arbiter_data.users WHERE auth0_id = auth0id);
+    CALL arbiter_data.read_user(auth0id, userid);
 END;
 GRANT EXECUTE ON PROCEDURE arbiter_data.get_current_user_info TO 'select_rbac'@'localhost';
 GRANT EXECUTE ON FUNCTION arbiter_data.get_organization_name TO 'select_rbac'@'localhost';
 GRANT EXECUTE ON PROCEDURE arbiter_data.get_current_user_info TO 'apiuser'@'%';
+/*
+    SELECT get_organization_name(organization_id) as organization,
+        BIN_TO_UUID(organization_id, 1) as organization_id,
+        BIN_TO_UUID(id, 1) as user_id, auth0_id
+    FROM arbiter_data.users WHERE auth0_id = auth0id;
+*/
 
 
 /*
@@ -363,45 +464,3 @@ BEGIN
 END;
 GRANT EXECUTE ON PROCEDURE arbiter_data.user_exists TO 'select_rbac'@'localhost';
 GRANT EXECUTE ON PROCEDURE arbiter_data.user_exists TO 'apiuser'@'%';
-
-
-/*
- * Drop and redefine add_permission_to_role to limit ability to add
- * rbac permissions to roles assigned to external users
- */
-DROP PROCEDURE add_permission_to_role;
-
-CREATE DEFINER = 'insert_rbac'@'localhost' PROCEDURE add_permission_to_role (
-    IN auth0id VARCHAR(32), IN role_id CHAR(36), IN permission_id CHAR(36))
-COMMENT 'Add an permission to the role permission mapping table'
-MODIFIES SQL DATA SQL SECURITY DEFINER
-BEGIN
-    DECLARE allowed BOOLEAN DEFAULT FALSE;
-    DECLARE roleid BINARY(16);
-    DECLARE permid BINARY(16);
-    DECLARE userorg BINARY(16);
-    SET userorg = get_user_organization(auth0id);
-    SET roleid = UUID_TO_BIN(role_id, 1); 
-    SET permid = UUID_TO_BIN(permission_id, 1); 
-    -- Check if user has update permission on the role and that
-    -- role and permission have same organization
-    SET allowed = can_user_perform_action(auth0id, roleid, 'update') AND 
-        userorg = get_object_organization(permid, 'permissions') AND 
-        userorg = get_object_organization(roleid, 'roles');
-    IF allowed IS NOT NULL AND allowed THEN
-        -- Don't insert an rbac permission when a role is granted externally 
-        IF rbac_permissions_check(permid) AND role_granted_to_external_users(roleid) THEN
-            SIGNAL SQLSTATE '42000' SET MESSAGE_TEXT = 'Cannot add administrative permissions to role of external users',
-            MYSQL_ERRNO = 1142;
-        ELSE
-            INSERT INTO arbiter_data.role_permission_mapping (
-                role_id, permission_id) VALUES (roleid, permid);
-        END IF;
-    ELSE
-        SIGNAL SQLSTATE '42000' SET MESSAGE_TEXT = 'Access denied to user on "add permission to role"',
-        MYSQL_ERRNO = 1142;
-    END IF; 
-END;
-
-GRANT EXECUTE ON PROCEDURE arbiter_data.add_permission_to_role TO 'insert_rbac'@'localhost';
-GRANT EXECUTE ON PROCEDURE arbiter_data.add_permission_to_role TO 'apiuser'@'%';
