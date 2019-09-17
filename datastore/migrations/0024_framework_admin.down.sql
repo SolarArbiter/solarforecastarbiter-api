8e51a2e8741b7768645e7f3b4540740f7f355aff
Leland Boeman <lboeman@email.arizona.edu>
2019-09-16 17:52:55 -0700

    fix 24 down

--- datastore/migrations/0024_framework_admin.down.sql
+++ datastore/migrations/0024_framework_admin.down.sql
@@ -11,7 +11,7 @@ DROP PROCEDURE IF EXISTS arbiter_data.promote_user_to_org_admin;
 DROP PROCEDURE IF EXISTS arbiter_data.add_user_to_org;
 DROP PROCEDURE IF EXISTS arbiter_data.delete_user;
 DROP PROCEDURE IF EXISTS arbiter_data.move_user_to_unaffiliated;
-DROP PROCEDURE IF EXISTS arbiter_data.remove_org_roles_from_user;
+DROP PROCEDURE IF EXISTS arbiter_data.remove_external_org_roles_from_user;
 
 DROP FUNCTION IF EXISTS arbiter_data.get_unaffiliated_orgid;
 DROP FUNCTION IF EXISTS arbiter_data.get_org_role_by_name;
