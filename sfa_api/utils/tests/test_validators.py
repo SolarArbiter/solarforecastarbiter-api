825a10135eca5389eaff0836e451251d06c041ae
Tony Lorenzo <atlorenzo@email.arizona.edu>
2019-10-11 10:56:00 -0700

    add timelimit validator for mysql timestamp limits

--- sfa_api/utils/tests/test_validators.py
+++ sfa_api/utils/tests/test_validators.py
@@ -1,5 +1,9 @@
+import datetime as dt
+
+
 from marshmallow.exceptions import ValidationError
 import pytest
+import pytz
 
 
 from sfa_api.utils import validators
@@ -39,3 +43,35 @@ def test_userstring(thestring):
 def test_invalid_userstring(thestring):
     with pytest.raises(ValidationError):
         validators.UserstringValidator()(thestring)
+
+
+@pytest.mark.parametrize('tz', [
+    'America/Phoenix',
+    'Etc/GMT+7'
+])
+def test_timezonevalidator(tz):
+    assert validators.TimezoneValidator()(tz) == tz
+
+
+@pytest.mark.parametrize('tz', ['PDT', 'Germany/Berlin'])
+def test_timezonevalidator_fail(tz):
+    with pytest.raises(ValidationError):
+        validators.TimezoneValidator()(tz)
+
+
+@pytest.mark.parametrize('time_', [
+    dt.datetime(2019, 1, 1, 12, 3, tzinfo=pytz.timezone('MST')),
+    dt.datetime(2019, 1, 1, 12, 3),
+    dt.datetime(1969, 12, 31, 17, 0, 1, tzinfo=pytz.timezone('MST')),
+])
+def test_timelimit_validator(time_):
+    assert validators.TimeLimitValidator()(time_) == time_
+
+
+@pytest.mark.parametrize('time_', [
+    dt.datetime(2049, 1, 1, 12, 3),
+    dt.datetime(1969, 12, 31, 14, 0, 1, tzinfo=pytz.timezone('MST')),
+])
+def test_timelimit_validator_fail(time_):
+    with pytest.raises(ValidationError):
+        validators.TimeLimitValidator()(time_)
--- sfa_api/utils/validators.py
+++ sfa_api/utils/validators.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import re
 import time
 
@@ -46,4 +47,27 @@ class TimezoneValidator(Validator):
     def __call__(self, value):
         if value not in ALLOWED_TIMEZONES:
             raise ValidationError('Invalid timezone.')
+        return value
+
+
+MAX_SQL_TIME = dt.datetime(2038, 1, 19, 3, 14, 7, 0, tzinfo=pytz.utc)
+MIN_SQL_TIME = dt.datetime(1970, 1, 1, 0, 0, 1, 0, tzinfo=pytz.utc)
+
+
+class TimeLimitValidator(Validator):
+    """
+    Ensures a DateTime is not outside the range of allowed for MySQL TIMESTAMP
+    objects
+    """
+    def __call__(self, value):
+        if value.tzinfo is not None:
+            testval = value.astimezone(pytz.utc)
+        else:
+            testval = pytz.utc.localize(value)
+        if testval > MAX_SQL_TIME:
+            raise ValidationError(
+                f'Exceeds maximum allowed timestamp of {MAX_SQL_TIME}')
+        elif testval < MIN_SQL_TIME:
+            raise ValidationError(
+                f'Less than minimum allowed timestamp of {MIN_SQL_TIME}')
         return value
