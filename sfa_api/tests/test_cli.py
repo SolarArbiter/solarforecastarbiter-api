6946d15e9bbdafffa91dfd3c16fcac247ac37bec
Tony Lorenzo <atlorenzo@email.arizona.edu>
2019-11-21 08:00:35 -0700

    add cli tests

--- sfa_api/tests/test_cli.py
+++ sfa_api/tests/test_cli.py
@@ -30,3 +30,26 @@ def test_devserver(mocker):
     r = runner.invoke(cli.cli, ['devserver'])
     assert r.exit_code == 0
     app.assert_called
+
+
+def test_scheduled_worker(mocker):
+    w = mocker.patch('rq.Worker')
+    runner = CliRunner()
+    with tempfile.NamedTemporaryFile('w') as f:
+        f.write('SCHEDULER_QUEUE = "scheduled"')
+        f.flush()
+        r = runner.invoke(cli.cli, ['scheduled-worker', f.name])
+    assert r.exit_code == 0
+    w.assert_called
+
+
+def test_scheduler(mocker):
+    run = mocker.patch('rq_scheduler.Scheduler.run')
+    runner = CliRunner()
+    with tempfile.NamedTemporaryFile('w') as f:
+        f.write('USE_FAKE_REDIS = True\n')
+        f.write('SCHEDULER_QUEUE = "scheduled"')
+        f.flush()
+        r = runner.invoke(cli.cli, ['scheduler', f.name])
+    assert r.exit_code == 0
+    run.assert_called
