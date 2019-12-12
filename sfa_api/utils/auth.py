d3b9931de489e2157b093aa189ef0d34df96ae2d
Leland Boeman <lboeman@email.arizona.edu>
2019-12-12 11:13:09 -0700

    use requests session and urllib3 Retry to handle retries

--- sfa_api/utils/auth.py
+++ sfa_api/utils/auth.py
@@ -8,6 +8,7 @@ it is better documented and is not missing any JWT features.
 from json.decoder import JSONDecodeError
 from functools import wraps
 import requests
+from urllib3 import Retry
 
 
 from flask import (request, Response, current_app, render_template,
@@ -51,32 +52,39 @@ def verify_access_token():
         return validate_user_existence()
 
 
-def request_user_info(retries=5):
+def request_user_info():
     """Makes a user info request to check if the user exists
     and is verified.
     Returns
     -------
     unser_info: dict
-        Dict of user information provided by auth0 or empty dict on failure.
+        Dict of user information provided by auth0.
     Raises
     ------
     json.decoder.JSONDecodeError
         If the user info request returns some invalid json.
+
+    requests.exceptions.HTTPError
+        If the request fails after 5 retries.
     """
-    if retries < 0:
-        return {}
-    info_request = requests.get(
-        current_app.config['AUTH0_BASE_URL'] + '/userinfo',
-        headers={
-            'Authorization': f'Bearer {current_access_token}',
-        })
-    try:
-        info_request.raise_for_status()
-    except requests.exceptions.HTTPError:
-        if info_request.status_code == 401:
-            # User is not authorized
-            return {}
-        return request_user_info(retries - 1)
+    session = requests.Session()
+    session.headers = {
+        'Authorization': f'Bearer {current_access_token}',
+    }
+    retries = Retry(
+        total=5, connect=3, read=3, status=3,
+        status_forcelist=[408, 500, 502, 503, 504],
+        backoff_factor=0.2,
+        respect_retry_after_header=True,
+    )
+    base_url = current_app.config['AUTH0_BASE_URL']
+    adapter = requests.adapters.HTTPAdapter(max_retries=retries)
+    session.mount(base_url, adapter)
+
+    info_request = session.get(
+        current_app.config['AUTH0_BASE_URL'] + '/userinfo', timeout=3.0)
+
+    info_request.raise_for_status()
     user_info = info_request.json()
     return user_info
 
@@ -102,9 +110,7 @@ def validate_user_existence():
     if not storage.user_exists():
         try:
             info = request_user_info()
-        except JSONDecodeError:
-            # The user has been removed from auth0 but the
-            # token is still valid
+        except (requests.exceptions.HTTPError, JSONDecodeError):
             return False
         else:
             if not info.get('email_verified', False):
