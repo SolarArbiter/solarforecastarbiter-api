a1ba219374d66424aa14036d2b18ffe48fe19dfc
Leland Boeman <lboeman@email.arizona.edu>
2020-04-29 10:27:13 -0700

    add forecast_type to object_pair schema

--- sfa_api/schema.py
+++ sfa_api/schema.py
@@ -43,6 +43,8 @@ INTERVAL_LABELS = ['beginning', 'ending', 'instant', 'event']
 AGGREGATE_TYPES = ['sum', 'mean', 'median', 'max', 'min']
 INTERVAL_VALUE_TYPES = ['interval_mean', 'interval_max', 'interval_min',
                         'interval_median', 'instantaneous']
+FORECAST_TYPES = ['forecast', 'event_forecast', 'probabilistic_forecast',
+                  'probabilistic_forecast_constant_value']
 
 EXTRA_PARAMETERS_FIELD = ma.String(
     title='Extra Parameters',
@@ -755,6 +757,12 @@ class ReportObjectPair(ma.Schema):
         allow_none=True,
         missing=None,
         validate=UncertaintyValidator(),
+    )
+    forecast_type = ma.String(
+        title='Forecast type',
+        description='The type of forecast represented in the pair.',
+        missing='forecast',
+        validate=validate.OneOf(FORECAST_TYPES)
     )
 
 
