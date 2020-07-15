8a0b0812c7d28eec1105aea6962eee36a7427062
Leland Boeman <lboeman@email.arizona.edu>
2020-07-15 13:31:27 -0700

    Mesage flashing safety (#330)
    * pass messages through safe filter
    
    * escape referrer header before cusing it in a message
    
    * flake8
    
    * add test for 404 handling
    
    * update tests for passing methods through either method

--- sfa_dash/api_interface/util.py
+++ sfa_dash/api_interface/util.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, escape
 
 from sfa_dash.errors import DataRequestException
 
@@ -49,8 +49,8 @@ def handle_response(request_object):
             }
             if previous_page is not None and previous_page != request.url:
                 errors['404'] = errors['404'] + (
-                    f' <a href="{previous_page}">Return to the previous '
-                    'page.</a>')
+                    f' <a href="{escape(previous_page)}">Return to the '
+                    'previous page.</a>')
         elif request_object.status_code == 422:
             errors = request_object.json()['errors']
         if errors:
--- sfa_dash/tests/views/test_api_404.py
+++ sfa_dash/tests/views/test_api_404.py
@@ -167,3 +167,27 @@ def test_clone_routes(client, clone_route, missing_id):
     resp = client.get(clone_route(ids), base_url=BASE_URL,
                       follow_redirects=True)
     assert_contains_404(resp.data.decode('utf-8'))
+
+
+@pytest.fixture(params=['/sites/{missing_id}/', '/admin/users/{missing_id}'])
+def referer_test_route(request, missing_id):
+    return request.param.format(missing_id=missing_id)
+
+
+@pytest.mark.parametrize('referer,expected', [
+    ('https://dashboard.solarforecastarbiter.org/sites/',
+     '<a href="https://dashboard.solarforecastarbiter.org/sites/">'
+     'Return to the previous page.</a>'),
+    ('https://dashboard.solarforecastarbiter.org/sites/'
+     '?a="></a><script>console.log("test")</script><a>',
+     '<a href="https://dashboard.solarforecastarbiter.org/sites/'
+     '?a=&#34;&gt;&lt;/a&gt;&lt;script&gt;console.log(&#34;test&#34;)&lt;'
+     '/script&gt;&lt;a&gt;">Return to the previous page.</a>'),
+])
+def test_previous_page_link(client, referer, expected, referer_test_route):
+    resp = client.get(referer_test_route,
+                      base_url=BASE_URL,
+                      query_string={'site_id': dne_uuid},
+                      headers={'Referer': referer})
+    assert_contains_404(resp.data.decode('utf-8'))
+    assert expected in resp.data.decode('utf-8')
