8c26f97b362c71af0106de7ab46c8d4c49b14345
lboeman <lboeman@email.arizona.edu>
2021-11-02 15:52:09 -0700

    consistent arg ordering, update comment

--- sfa_api/admincli.py
+++ sfa_api/admincli.py
@@ -469,13 +469,14 @@ def reference_persistence_job(
 @click.argument('copy_from', type=click.UUID)
 @click.argument('copy_to', type=click.UUID)
 def trial_data_copy_job(
-        name, user_id, cron_string, base_url, timeout, copy_from, copy_to,
+        name, user_id, cron_string, timeout, copy_from, copy_to, base_url,
         **kwargs):
     """
-    Create a job to copy latest data from one observation to another. Cron
-    should be the observation interval length for short intervals, but
-    should be carefully considered for longer intervals and be scheduled
-    shortly after data is posted.
+    Create a job to copy latest data from one observation to another.
+    For short intervals, cron_string should schedule execution every
+    x minutes where x is the observation interval length. For long
+    intervals, cron string should should be set to execute shortly
+    after data is posted to avoid introducing high latency.
     """
     from sfa_api.jobs import create_job
     id_ = create_job(
