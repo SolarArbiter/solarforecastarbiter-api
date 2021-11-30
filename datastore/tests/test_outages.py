73827061b3c95e4bdf2de22aac1f81ae97442f0c
lboeman <lboeman@email.arizona.edu>
2021-11-30 10:58:00 -0700

    fix sql test

--- datastore/tests/test_outages.py
+++ datastore/tests/test_outages.py
@@ -11,6 +11,7 @@ def add_perm(cursor, new_permission, valueset):
     # Add a permission to user0, role0 of valueset
     role_id = valueset[2][0]['id']
     org = valueset[0][0]
+
     def fcn(action, what):
         perm = new_permission(action, what, True, org=org)
         cursor.execute(
@@ -28,7 +29,10 @@ def one_system_outage(cursor):
 
     cursor.execute(
         'CALL store_system_outage(%s, %s, %s)',
-        (some_uuid, start.strftime('%Y-%m-%d %H:%M'), end.strftime('%Y-%m-%d %H:%M'))
+        (some_uuid,
+         start.strftime('%Y-%m-%d %H:%M'),
+         end.strftime('%Y-%m-%d %H:%M')
+         )
     )
     return some_uuid
 
@@ -45,7 +49,10 @@ def test_add_remove_system_otuage(cursor):
 
     cursor.execute(
         'CALL store_system_outage(%s, %s, %s)',
-        (some_uuid, start.strftime('%Y-%m-%d %H:%M'), end.strftime('%Y-%m-%d %H:%M'))
+        (some_uuid,
+         start.strftime('%Y-%m-%d %H:%M'),
+         end.strftime('%Y-%m-%d %H:%M')
+         )
     )
     cursor.execute('CALL list_system_outages()')
     outages = cursor.fetchall()
@@ -71,7 +78,7 @@ def test_report_outage_no_perms(cursor, valueset, new_report):
     some_uuid = bin_to_uuid(newuuid())
     start = datetime.datetime(2021, 4, 14, 12, 0)
     end = datetime.datetime(2021, 4, 14, 13, 0)
-    with pytest.raises(pymysql.err.OperationalError) as e:
+    with pytest.raises(pymysql.err.OperationalError):
         cursor.execute(
             "CALL store_report_outage(%s, %s, %s, %s, %s)",
             (auth0_id, report_id, some_uuid,
@@ -122,16 +129,23 @@ def test_delete_report_outage(cursor, valueset, new_report, add_perm):
     start = datetime.datetime(2021, 4, 14, 12, 0)
     end = datetime.datetime(2021, 4, 14, 13, 0)
     cursor.execute(
-        "INSERT INTO report_outages (id, report_id, start, end) VALUES (%s, %s, %s, %s)",
-        (report_id, some_uuid,
+        "INSERT INTO report_outages (id, report_id, start, end) "
+        "VALUES (%s, %s, %s, %s)",
+        (some_uuid, report_id,
          start.strftime('%Y-%m-%d %H:%M'),
          end.strftime('%Y-%m-%d %H:%M'))
     )
-    cursor.execute("SELECT * FROM report_outages WHERE report_id = %s", (report_id,))
+    cursor.execute(
+        "SELECT * FROM report_outages WHERE report_id = %s",
+        (report_id,)
+    )
     assert len(cursor.fetchall()) == 1
     cursor.execute(
         "CALL delete_report_outage(%s, %s, %s)",
-        (auth0_id, report_id, some_uuid)
+        (auth0_id, bin_to_uuid(report_id), bin_to_uuid(some_uuid))
     )
-    cursor.execute("SELECT * FROM report_outages WHERE report_id = %s", (report_id,))
+    cursor.execute(
+        "SELECT * FROM report_outages WHERE report_id = %s",
+        (report_id,)
+    )
     assert len(cursor.fetchall()) == 0
